d7538fb8e3d197386a985cb77f401f705bf0bd46
MarleneBarahona <00118616@uca.edu.sv>
2020-10-14 15:57:37 -0600

    Visualizando listado de Donantes en mantenimiento.

--- src/main/java/com/pp/ycht/controller/AdminController.java
+++ src/main/java/com/pp/ycht/controller/AdminController.java
@@ -1,8 +1,33 @@
 package com.pp.ycht.controller;
 
+import com.pp.ycht.domain.Donante;
+import com.pp.ycht.repo.IDonanteRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class AdminController {
 
+    @Autowired
+    private IDonanteRepo repo;
+
+    @RequestMapping("/admin")
+    public String greeting() {
+        /*Donante donante = new Donante();
+
+        //CREATE
+        donante.setIdDonante(50);
+        donante.setNombreDonante("pruebita");
+        repo.save(donante);*/
+
+        return "admin/mantenimiento";
+    }
+
+    @RequestMapping("/verDonantes")
+    public String verDonantes(Model model) {
+        model.addAttribute("donantes", repo.findAll());
+        return "admin/donantes/mantenimientoVerDonantes";
+    }
 }

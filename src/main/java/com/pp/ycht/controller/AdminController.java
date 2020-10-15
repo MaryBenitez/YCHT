9b2e3c3aa48c5a39b91609b0750c2f5e765f9938
MarleneBarahona <00118616@uca.edu.sv>
2020-10-14 18:00:46 -0600

    Elimina Donante enviando el id como parametro en la URL y lo actualiza.

--- src/main/java/com/pp/ycht/controller/AdminController.java
+++ src/main/java/com/pp/ycht/controller/AdminController.java
@@ -5,7 +5,10 @@ import com.pp.ycht.repo.IDonanteRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class AdminController {
@@ -27,6 +30,13 @@ public class AdminController {
 
     @RequestMapping("/verDonantes")
     public String verDonantes(Model model) {
+        model.addAttribute("donantes", repo.findAll());
+        return "admin/donantes/mantenimientoVerDonantes";
+    }
+
+    @RequestMapping(value="/borrar/{id}", method= RequestMethod.GET)
+    public String borrar(@PathVariable("id") Integer id, Model model){
+        repo.delete(repo.getOne(id));
         model.addAttribute("donantes", repo.findAll());
         return "admin/donantes/mantenimientoVerDonantes";
     }
--- src/main/java/com/pp/ycht/repo/IDonanteRepo.java
+++ src/main/java/com/pp/ycht/repo/IDonanteRepo.java
@@ -2,6 +2,8 @@ package com.pp.ycht.repo;
 
 import com.pp.ycht.domain.Donante;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
 public interface IDonanteRepo extends JpaRepository<Donante,Integer> {
 }

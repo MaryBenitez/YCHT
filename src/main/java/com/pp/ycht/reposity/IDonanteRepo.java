df472ced9dc707f454f3fb5cf95041cd61a090cd
MarleneBarahona <00118616@uca.edu.sv>
2020-10-20 18:28:12 -0600

    Agregando prueba para filtros.

--- src/main/java/com/pp/ycht/reposity/IDonanteRepo.java
+++ src/main/java/com/pp/ycht/reposity/IDonanteRepo.java
@@ -3,5 +3,10 @@ package com.pp.ycht.reposity;
 import com.pp.ycht.domain.Donante;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface IDonanteRepo extends JpaRepository<Donante,Integer> {
+
+    List<Donante> findByLastname(String lastname);
+
 }
